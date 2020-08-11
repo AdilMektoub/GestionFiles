d8cc39edab944986af3474418baf154ce86521b7
adil <adil.mektoub@gmail.com>
2020-08-11 10:20:40 +0200

    Connexion et affichage de la BDD

--- Test.java
+++ Test.java
@@ -1,18 +1,28 @@
 package StringBufferBuilder;
 
+import java.awt.EventQueue;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
 import com.mysql.fabric.xmlrpc.base.Array;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 public class Test {
 
+	//Déclaration variable Static de la methode affichage
+	static String url = "jdbc:mysql://localhost:3306/bib";
+	static  String user = "root";
+	static String passwd = "";
+	
 	public static void main(String[] args) {
 		
 
@@ -115,11 +125,53 @@ public class Test {
 		};
 		file2.Lire(f2, stagiaires);
 		
-		
-		
-			
+		//connection à la BDD
+    	EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                // Votre fonction Run
+                // Vos information de connexion à une base de données
+                String BDD = "bib";
+                String url = "jdbc:mysql://localhost:3306/" + BDD;
+                String user = "root";
+                String passwd = "";
+                
+                // L'essaie de connexion à votre base de donées
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection conn =
+                            (Connection) DriverManager.getConnection(url, user, passwd);
+                    System.out.println("Connecter");
+                    //si ça se connecte pas exception
+                } catch (Exception e){
+                    e.printStackTrace();
+                    System.out.println("Erreur");
+                    System.exit(0);
+                }
+            }
+        });
+    	
+    			
 	}
-		
+	//Affichage d'élément de la BDD	
+	public static ArrayList<String> affichageUtilisateurs(  ) { 
+		ArrayList<String> list = new ArrayList<String>();
+		try { Connection connection = 
+				(Connection) DriverManager.getConnection( url, user, passwd ) ;
+			//String strSql = "SELECT * FROM T_Users WHERE login='" + login + "' AND password='" + password + "'";
+			String strSql = "SELECT * FROM Utilisateur"; //  WHERE login=? AND password=?
+			try ( PreparedStatement statement  = (PreparedStatement) connection.prepareStatement( strSql ) ) {
+//				statement.setString( 1, login );
+//				statement.setString( 2, password );
+				try ( ResultSet resultSet = statement.executeQuery() ) {
+					while ( resultSet.next() ) { //(int id, int id_livre, String nom, String login, String password)	
+								list.add(resultSet.getString( "titre" )); }
+				}
+			}
+		} catch ( Exception exception ) {
+			throw new RuntimeException( exception );
+		}
+		return list;
+	}
 }
 
 
